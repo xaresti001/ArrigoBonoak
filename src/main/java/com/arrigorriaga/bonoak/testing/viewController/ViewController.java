fac6f7c2dbdf1ecf2dbd215ce5dfca59bcc851f5
Xabier Aresti <xabier.aresti@gmail.com>
2021-01-21 13:43:46 +0100

    20210121 - V1 Added User-update functionality - Thymeleaf View Model

--- src/main/java/com/arrigorriaga/bonoak/testing/viewController/ViewController.java
+++ src/main/java/com/arrigorriaga/bonoak/testing/viewController/ViewController.java
@@ -23,12 +23,12 @@ public class ViewController {
         this.roleService = roleService;
     }
 
-    @GetMapping("/managerPanel")
-    public String managerPanel(ModelMap model){
+    @GetMapping("/managerPanel/")
+    public String managerPanel(){
         return "managerPanel";
     }
 
-    @GetMapping("/managerPanel/allUsers")
+    @GetMapping("/managerPanel/allUsers/")
     public String getAllUsers(ModelMap model){
         List<User> allUsers = userService.findAllUsers();
         List<Role> allRoles = roleService.findAllRoles();
@@ -42,28 +42,49 @@ public class ViewController {
     }
 
 
-    @GetMapping("/managerPanel/allUsers/searchUser")
+    @GetMapping("/managerPanel/allUsers/searchUser/")
     public String searchUserById(@RequestParam("search") int userId, ModelMap model){
         User searchUser = userService.findUserById(userId);
         model.addAttribute("searchUser", searchUser);
         return "viewSearchUsers";
     }
 
-    @PostMapping("/managerPanel/allUsers/deleteUser")
+    @PostMapping("/managerPanel/allUsers/deleteUser/")
     public String deleteUserById(@ModelAttribute("userForm") User user, ModelMap model){
         boolean control = userService.deleteUserById(user.getId());
         System.out.println(control);
         System.out.println(user.getId());
         model.clear();
-        return "redirect:/view/managerPanel/allUsers";
+        return "redirect:/view/managerPanel/allUsers/";
     }
 
-    @PostMapping("/managerPanel/allUsers/createUser")
+    @PostMapping("/managerPanel/allUsers/createUser/")
     public String createUser(@ModelAttribute User user, ModelMap model){
-        User tempUser = userService.createUser(user);
+        userService.createUser(user);
         model.clear();
-        return "redirect:/view/managerPanel/allUsers";
+        return "redirect:/view/managerPanel/allUsers/";
     }
+
+    @GetMapping("/managerPanel/allUsers/updateUser/")
+    public String updateUser(@ModelAttribute User user, ModelMap model){
+        User tempUser = userService.findUserById(user.getId());
+        model.addAttribute("searchUser", tempUser);
+        List<Role> allRoles = roleService.findAllRoles();
+        model.addAttribute("allRoles", allRoles);
+
+        // En este caso no purgamos la vista, porque queremos mantener el atributo local.
+        // El atributo local visualizará en el formulario la información del usuario.
+        return "viewUpdateUser";
+    }
+
+    @PostMapping("/managerPanel/allUsers/updateUser/update/")
+    public String updateUserConfirm(@ModelAttribute("searchUser") User user, ModelMap model){
+        userService.updateUser(user);
+        model.clear(); // En esta caso purgamos la vista y volvemos al punto de partida.
+        return "redirect:/view/managerPanel/allUsers/";
+    }
+
+
 
 
 }
